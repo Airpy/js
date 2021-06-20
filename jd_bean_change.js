4820a682cd2f2dcfed7426bf584d695abd0c1270
knowledgeablelmh <83534987+knowledgeablelmh@users.noreply.github.com>
2021-06-20 23:47:55 +0800

    Update jd_bean_change.js

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -12,13 +12,13 @@ Modified time: 2021-06-9 15:25:41
 55 23 * * * jd_bean_change.js, tag=京东资产变动通知, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 ================Loon===============
 [Script]
-cron "2 9 * * *" script-path=jd_bean_change.js, tag=京东资产变动通知
+cron "55 23 * * *" script-path=jd_bean_change.js, tag=京东资产变动通知
 =============Surge===========
 [Script]
-京东资产变动通知 = type=cron,cronexp="2 9 * * *",wake-system=1,timeout=3600,script-path=jd_bean_change.js
+京东资产变动通知 = type=cron,cronexp="55 23 * * *",wake-system=1,timeout=3600,script-path=jd_bean_change.js
 
 ============小火箭=========
-京东资产变动通知 = type=cron,script-path=jd_bean_change.js, cronexpr="2 9 * * *", timeout=3600, enable=true
+京东资产变动通知 = type=cron,script-path=jd_bean_change.js, cronexpr="55 23 * * *", timeout=3600, enable=true
  */
 const $ = new Env('京东资产变动通知');
 const notify = $.isNode() ? require('./sendNotify') : '';
